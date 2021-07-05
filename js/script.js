ddb7c3126ec56ab33ef03af4536c17329a05256b
Michal Pawel Wadowski <80638034+Michau94@users.noreply.github.com>
2021-07-05 14:06:56 +0200

    fizz buzz js + print on html

---
+++ js/script.js
@@ -0,0 +1,41 @@
+/* Scrivi un programma che stampi i numeri da 1 a 100,
+ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
+Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz. */
+
+// 1 - costruire ciclo for che conti fino a 100
+// 2 - if else con modulo
+//2a - log per controllo; 
+// 3 - stampa su html
+// pro tip - usare sempre variabile di appoggio per stampare più elementi di un ciclo for su html altrimenti verranno sovrascritti
+
+//variables
+var display = document.getElementById('count');
+
+//variabile di appoggio
+var numberPrint = '';
+
+//for cycle 
+
+for (var i = 1; i <= 100; i++) {
+
+    //conditions
+
+    if (i % 3 === 0 && i % 5 === 0) {
+        numberPrint += '<li>' + 'FizzBuzz' + '</li>';
+        console.log('FizzBuzz');
+    } else if (i % 3 === 0) {
+        console.log('Fizz');
+        numberPrint += '<li>' + 'Fizz' + '</li>';
+    }
+    else if (i % 5 === 0) {
+        console.log('Buzz');
+        numberPrint += '<li>' + 'Buzz' + '</li>';
+    } else {
+        numberPrint += '<li>' + i + '</li>';
+        console.log(i);
+    }
+}
+
+//output
+
+display.innerHTML = numberPrint;
